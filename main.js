ce329c8b3e3d82cd59b87519030fc04710381155
Ace Fedora <37934171+ItsJustAce@users.noreply.github.com>
2023-01-30 16:44:46 +0000

    fix spelling mistake

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <footer>
-                sace aka retraace aka return 2 swagging and dabbing aka just kill the cringe for everyones sake / <a href="archive.html">site ver. 2.0</a> / <a href="index.html">return home</a>
+                ace aka retraace aka return 2 swagging and dabbing aka just kill the cringe for everyones sake / <a href="archive.html">site ver. 2.0</a> / <a href="index.html">return home</a>
             </footer>
         `
     }
